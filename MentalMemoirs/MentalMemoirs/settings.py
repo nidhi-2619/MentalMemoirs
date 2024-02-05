9bb9a64d9fe70f51abf573937e0755d6c24c9c8b
nidhi-2619 <91557785+nidhi-2619@users.noreply.github.com>
2024-02-06 02:16:37 +0530

    feat(core): core app added with few packages to handle custom commands and customization

--- MentalMemoirs/MentalMemoirs/settings.py
+++ MentalMemoirs/MentalMemoirs/settings.py
@@ -13,10 +13,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv
 import os
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -24,8 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.getenv('SECRET_KEY'))
+DEBUG = str(os.getenv('DEBUG') == 'True')
 
 ALLOWED_HOSTS = []
 
